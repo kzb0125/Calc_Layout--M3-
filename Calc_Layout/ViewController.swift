45b7205cdf2519bdd35fc0edac958901fbf69f61
kzb0125 <kzb0125@auburn.edu>
2024-01-24 16:37:49 -0800

    Added button func

--- Calc_Layout/ViewController.swift
+++ Calc_Layout/ViewController.swift
@@ -9,11 +9,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var calcWorkings: UILabel!
+    @IBOutlet weak var calcResults: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func allClearTap(_ sender: Any) {
+    }
+    
+    @IBAction func backTap(_ sender: Any) {
+    }
+    
+    @IBAction func signTap(_ sender: Any) {
+    }
+    
+    @IBAction func percentTap(_ sender: Any) {
+    }
+    
+    @IBAction func divideTap(_ sender: Any) {
+    }
+    
+    @IBAction func multiplyTap(_ sender: Any) {
+    }
+    
+    @IBAction func minusTap(_ sender: Any) {
+    }
+    
+    @IBAction func addTap(_ sender: Any) {
+    }
+    
+    @IBAction func equalTap(_ sender: Any) {
+    }
+    
+    @IBAction func decimalTap(_ sender: Any) {
+    }
+   
+    @IBAction func zeroTap(_ sender: Any) {
+    }
+    
+    @IBAction func oneTap(_ sender: Any) {
+    }
+    
+    @IBAction func twoTap(_ sender: Any) {
+    }
+    
+    @IBAction func threeTap(_ sender: Any) {
+    }
+    
+    @IBAction func fourTap(_ sender: Any) {
+    }
+    
+    @IBAction func fiveTap(_ sender: Any) {
+    }
+    
+    @IBAction func sixTap(_ sender: Any) {
+    }
+    
+    @IBAction func sevenTap(_ sender: Any) {
+    }
+    
+    @IBAction func eightTap(_ sender: Any) {
+    }
+    
+    @IBAction func nineTap(_ sender: Any) {
+    }
+    
 }
 
